f9ab0befc406a2a580ba8660b6c62e4c88e5691f
yanaivantsova <yanaivantsova@yandex.by>
2023-04-17 15:32:30 +0200

    feat:add solution the sort-by-height-task

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,11 +11,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  const numberToDelete = -1;
+  let  sortedArray;
+  let filteredArray = arr.filter((number) => number !== numberToDelete);
+  function quickSort(notSortedArray) {
+    if (notSortedArray.length == 0) return [];
+    let a = [], b = [], p = notSortedArray[0];
+    for (let i = 1; i < notSortedArray.length; i++) {
+      if (notSortedArray[i] < p) a.push(notSortedArray[i]);
+      else b.push(notSortedArray[i]);
+    }
+    return quickSort(a).concat(p, quickSort(b));
+  }
+  sortedArray = quickSort(filteredArray);
+   let indexesOfNumber  = [];
+  let idx = arr.indexOf(numberToDelete);
+  while (idx != -1) {
+    indexesOfNumber.push(idx);
+    idx = arr.indexOf(numberToDelete, idx + 1);
+  }
+ for(let i=0; i<indexesOfNumber.length;i++){
+  sortedArray.splice(indexesOfNumber[i],0,numberToDelete);
 }
-
+return sortedArray;
+}
 module.exports = {
   sortByHeight
 };
