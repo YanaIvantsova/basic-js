e27e9f80625da4ccf6f71c130a4887ab62c79ef5
yanaivantsova <yanaivantsova@yandex.by>
2023-12-20 20:38:00 +0100

    feat: add dns-stats task solution

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/*domains*/ ) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let obj = {}, parts, part = '';
+  domains.forEach(element => {
+    parts = element.split('.').reverse();
+    part = '';
+    parts.forEach((item) => {
+      part += `.${item}`;
+      obj[part] = obj[part] ? obj[part] + 1 : 1;
+    })
+  });
+  return obj;
 }
 
 
